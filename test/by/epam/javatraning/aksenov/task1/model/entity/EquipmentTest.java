eb7b4866989f81295480b4c2924b39897684bc36
Aksenov <000galaxyy000@gmail.com>
2019-02-28 18:24:33 +0300

    EquipmentTest has been updated

--- test/by/epam/javatraning/aksenov/task1/model/entity/EquipmentTest.java
+++ test/by/epam/javatraning/aksenov/task1/model/entity/EquipmentTest.java
@@ -14,48 +14,64 @@ public class EquipmentTest {
     }
 
     @Test
-    public void constructorEquipmentTest() {
-        Equipment equipment1 = new Equipment(120, 50, true);
-        Equipment equipment2 = new Equipment(-120, -50, true);
-        Equipment equipment3 = new Equipment(equipment1);
-        Equipment equipment4 = new Equipment(null);
+    public void testConstructorEquipment() {
+        Equipment equipment = new Equipment(120, 50, true);
 
         double expected1 = 120;
-        double actual1 = equipment1.getPrice();
+        double actual1 = equipment.getPrice();
 
         double expected2 = 50;
-        double actual2 = equipment1.getPower();
-
-        double expected3 = 0;
-        double actual3 = equipment2.getPrice();
-
-        double expected4 = 0;
-        double actual4 = equipment2.getPower();
-
-        double expected5 = 120;
-        double actual5 = equipment3.getPrice();
-
-        double expected6 = 50;
-        double actual6 = equipment3.getPower();
-
-        double expected7 = 0;
-        double actual7 = equipment4.getPrice();
-
-        double expected8 = 0;
-        double actual8 = equipment4.getPower();
-
-        boolean actual9 = equipment4.isSelector();
+        double actual2 = equipment.getPower();
 
         Assert.assertEquals(expected1, actual1);
         Assert.assertEquals(expected2, actual2);
-        Assert.assertEquals(expected3, actual3);
-        Assert.assertEquals(expected4, actual4);
-        Assert.assertEquals(expected5, actual5);
-        Assert.assertEquals(expected6, actual6);
-        Assert.assertEquals(expected7, actual7);
-        Assert.assertEquals(expected8, actual8);
-        Assert.assertFalse(actual9);
+    }
 
+    @Test
+    public void testConstructorEquipmentNegativeArgs() {
+        Equipment equipment = new Equipment(-120, -50, true);
+
+        double expected1 = 0;
+        double actual1 = equipment.getPrice();
+
+        double expected2 = 0;
+        double actual2 = equipment.getPower();
+
+        Assert.assertEquals(expected1, actual1);
+        Assert.assertEquals(expected2, actual2);
+
+    }
+
+    @Test
+    public void testConstructorEquipmentCopier() {
+        Equipment equipment = new Equipment(200.5, 650.9, false);
+        Equipment equipmentCopy = new Equipment(equipment);
+
+        double expected1 = 200.5;
+        double actual1 = equipmentCopy.getPrice();
+
+        double expected2 = 650.9;
+        double actual2 = equipmentCopy.getPower();
+
+        Assert.assertEquals(expected1, actual1);
+        Assert.assertEquals(expected2, actual2);
+    }
+
+    @Test
+    public void testConstructorNullArr() {
+        Equipment equipment4 = new Equipment(null);
+
+        double expected1 = 0;
+        double actual1 = equipment4.getPrice();
+
+        double expected2 = 0;
+        double actual2 = equipment4.getPower();
+
+        boolean actual3 = equipment4.isSelector();
+
+        Assert.assertEquals(expected1, actual1);
+        Assert.assertEquals(expected2, actual2);
+        Assert.assertFalse(actual3);
     }
 
     @Test(expectedExceptions = NegativeArgumentException.class)
